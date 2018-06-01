f9d259f60191d4f9b85d13c26b0dbc0436ba6368
Pietro Mascolo <pietro_mascolo@optum.com>
2018-06-01 22:46:12 +0100

    ADDS additional slides and code

---
+++ code/breast_cancer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from sklearn import datasets
+# info on dataset available at: https://goo.gl/U2Uwz2
+data = datasets.load_breast_cancer(return_X_y=False)
+
+# START OMIT
+df = pd.DataFrame(data['data'], columns=data['feature_names'])
+print(f"Dataset contains {df.shape[0]} rows x {df.shape[1]} columns")
+# END OMIT
+df.columns = [f"X{i:02d}" for i in range(len(data['feature_names']))]
+
+from pprint import pprint
+print("Column names:")
+pprint(list(df)) 
